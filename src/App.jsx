0cc03085ea8e067d7ce2dd33d865eb4179c35106
faisalchohan655-design <faisalchohan655@gmail.com>
2026-06-24 15:21:38 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { LeadsProvider } from './context/LeadsContext.jsx';
 import Layout from './components/layout/Layout';
-import Dashboard from './pages/Dashboard';
-import LeadFinder from './pages/LeadFinder';
-import EmailExtractor from './pages/EmailExtractor';
-import CampaignHub from './pages/CampaignHub';
+import Dashboard from './pages/Dashboard.jsx';
+import LeadFinder from './pages/LeadFinder.jsx';
+import EmailExtractor from './pages/EmailExtractor.jsx';
+import CampaignHub from './pages/CampaignHub.jsx';
+import LeadManager from './pages/LeadManager.jsx';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/finder" element={<LeadFinder />} />
             <Route path="/extractor" element={<EmailExtractor />} />
             <Route path="/campaign" element={<CampaignHub />} />
+            <Route path="/leads" element={<LeadManager />} />
           </Routes>
         </Layout>
         <Toaster 
